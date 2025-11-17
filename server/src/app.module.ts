e7cd6cf27fef87d25e758f059a18c4945ec559d5
mule2020 <mulgashaw12@gmail.com>
2025-11-16 20:09:41 -0500

    deployment setup env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,18 +7,24 @@ import { Budget } from './budgets/budget.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'expense_tracker',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Expense, Budget, User],
-      synchronize: true, // dev only
+      synchronize: true,
     }),
     ExpensesModule,
     BudgetsModule,
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,46 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-
-  app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    stopAtFirstError: false,
-  }),
-);
-
-  // Swagger config
-  const config = new DocumentBuilder()
-    .setTitle('MyPersonal Expense Tracker API')
-    .setDescription('NestJS API for expenses, budgets and auth')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'Authorization',
-        in: 'header',
-      },
-      'access-token', // name of the security scheme
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // React dev server
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: configService.get('CORS_ORIGIN'),
+    methods: '*',
+    credentials: true,
   });
 
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Personal Expense Tracker API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api-docs', app, swaggerDoc);
+
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
